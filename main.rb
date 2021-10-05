0dde6972a606fc790ae04c5da80e43b75636e04b
Brooke Wittenberg <bwittenberg.developer@gmail.com>
2021-10-05 12:35:58 -0500

    change indexes

--- main.rb
+++ main.rb
@@ -9,13 +9,13 @@ inspace_flattened = in_space.flatten
 
 rows = []
 
-rows << [inspace_flattened[1][0]["craft"], inspace_flattened[1][0]["name"]]
-rows << [inspace_flattened[1][1]["craft"], inspace_flattened[1][1]["name"]]
-rows << [inspace_flattened[1][2]["craft"], inspace_flattened[1][2]["name"]]
-rows << [inspace_flattened[1][3]["craft"], inspace_flattened[1][3]["name"]]
-rows << [inspace_flattened[1][4]["craft"], inspace_flattened[1][4]["name"]]
-rows << [inspace_flattened[1][5]["craft"], inspace_flattened[1][5]["name"]]
-rows << [inspace_flattened[1][6]["craft"], inspace_flattened[1][6]["name"]]
+rows << [inspace_flattened[5][0]["craft"], inspace_flattened[5][0]["name"]]
+rows << [inspace_flattened[5][1]["craft"], inspace_flattened[5][1]["name"]]
+rows << [inspace_flattened[5][2]["craft"], inspace_flattened[5][2]["name"]]
+rows << [inspace_flattened[5][3]["craft"], inspace_flattened[5][3]["name"]]
+rows << [inspace_flattened[5][4]["craft"], inspace_flattened[5][4]["name"]]
+rows << [inspace_flattened[5][5]["craft"], inspace_flattened[5][5]["name"]]
+rows << [inspace_flattened[5][6]["craft"], inspace_flattened[5][6]["name"]]
 table = Terminal::Table.new :rows => rows
 table = Terminal::Table.new :headings => ['Craft', 'Name'], :rows => rows
 
