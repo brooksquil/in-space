9308cf9423356f9aca07f1feb2373fae30a619b9
Brooke Wittenberg <bwittenberg.developer@gmail.com>
2021-10-08 09:53:59 -0500

    sorting by first name

--- main.rb
+++ main.rb
@@ -6,18 +6,23 @@ response = HTTParty.get(url)
 in_space = response.parsed_response
 
 inspace_flattened = in_space.flatten[5]
-
+inspace_grouped = inspace_flattened.group_by { |craft| craft.shift }
 rows =[]
-
-puts inspace_flattened.group_by { |craft| craft.shift }
 table = Terminal::Table.new do |t|
     t.rows = rows 
     t.add_row ["Craft", "Name"]  
     t << :separator 
-    inspace_flattened.each_with_index do | value, index |
+    inspace_grouped.each do |key, value|
+        puts value.sort_by{|person| person["name"]}
+        value.each do |people|
+
+            t.add_row [key[1],people["name"]]
+        end
         
-        t.add_row [value["craft"], value["name"]]
     end
+        
+    # t.add_row [value["craft"], value["name"]]
+    
     
 end
 
